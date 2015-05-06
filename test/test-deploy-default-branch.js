8272c4808cb797d52fe90c49d50085909b0fca5f
Sam Roberts <sam@strongloop.com>
2015-05-06 14:51:01 -0700

    Log name of service that was deployed to

--- test/test-deploy-default-branch.js
+++ test/test-deploy-default-branch.js
@@ -1,5 +1,6 @@
 var assert = require('assert');
 var childProcess = require('child_process');
+var concat = require('concat-stream');
 var helpers = require('./helpers');
 var shell = require('shelljs');
 
@@ -9,13 +10,20 @@ helpers.gitServer(test);
 function test(server, ci) {
   ci.once('commit', assertCommit);
 
-  childProcess.fork(
+  var child = childProcess.fork(
     require.resolve('../bin/sl-deploy'),
     [
       '--service', 's1',
       'http://any:thin@127.0.0.1:' + server.address().port
-    ]
+    ],
+    {silent: true}
   );
+
+  child.stderr.pipe(process.stderr);
+  child.stdout.pipe(concat(function(msg) {
+    console.log('Check deploy message: <%s>', String(msg).trim());
+    assert(/Deployed.*as `1` to `http.*/.test(msg), msg);
+  }));
 
   function assertCommit(commit) {
     var branch = 'deploy';
