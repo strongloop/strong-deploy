a07ededb5b3c8bd72318bb49cb753fc9c3be7345
Ryan Graham <r.m.graham@gmail.com>
2016-05-06 11:53:59 -0700

    update copyright notices and license

--- bin/sl-deploy.js
+++ bin/sl-deploy.js
@@ -1,4 +1,8 @@
 #!/usr/bin/env node
+// Copyright IBM Corp. 2014,2016. All Rights Reserved.
+// Node module: strong-deploy
+// This file is licensed under the Artistic License 2.0.
+// License text available at https://opensource.org/licenses/Artistic-2.0
 
 'use strict';
 
